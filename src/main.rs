d2b0fb9eeda37aa9253a041eb1925f4005370380
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-06 08:18:55 +0300

    added comments for else if conditions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
     let number = 3;
-
+    //conditions in if statements are sometimes referred to as arms
+    //when else if is used, program stops the moment the first true condition is met 
     if number < 5 {
         println!("the condition is true!");
+    }if number != 0 {
+        println!("the number is not equal to zero!");
     }else{
         println!("the condition is false!");
     }
