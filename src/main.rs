d34b1fb42a85dcab800c526858394a546f878120
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-06 08:51:59 +0300

    added both while and for loop examples

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 fn main() {
-    let mut number = 3;
+    let a = [10, 20, 30, 40, 50];
+    
+    /*while loop to iterate an array.
+      this could be error prone if the array length is incorrect
+      it is also slow since compiler has to add the conditional check for each element in array
+    */
+    let  mut index = 0;
 
-    while number != 0 {
-        println!("{}!", number);
+    while index < 5 {
+        println!("the value is: {}",a[index]);
 
-        number = number - 1;
+        index = index + 1;
     }
+    
+    println!("separator line for the two loop types");
 
-    println!("LIFTOFF!!!");
+    //for loop example for the same
+
+    for element in a.iter(){
+        println!("the value is: {}", element);
+    }
 }
