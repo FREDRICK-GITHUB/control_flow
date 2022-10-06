2c5cc2691113a0c20f6e4405d6715a354d2b3860
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-06 08:56:03 +0300

    added for loop with range example

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,6 @@
 fn main() {
-    let a = [10, 20, 30, 40, 50];
-    
-    /*while loop to iterate an array.
-      this could be error prone if the array length is incorrect
-      it is also slow since compiler has to add the conditional check for each element in array
-    */
-    let  mut index = 0;
-
-    while index < 5 {
-        println!("the value is: {}",a[index]);
-
-        index = index + 1;
+    for number in (1..4).rev(){ // loop starts from the first item in range and ending before the declared number in range
+        println!("{}!",number);
     }
-    
-    println!("separator line for the two loop types");
-
-    //for loop example for the same
-
-    for element in a.iter(){
-        println!("the value is: {}", element);
-    }
+    println!("LIFTOFF!!!");
 }
