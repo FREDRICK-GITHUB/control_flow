c69cf032fe047a60d86a233ab05d26141dd4eb26
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-06 08:30:47 +0300

    loop example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 fn main() {
-    let condition = true;
-
-    let number = if condition {
-        5
-    }else{
-        6
-    };
- //conditions of the arms in this if expression must be of the same type
-    println!("the value of number is: {}", number);
+    //loop example that repeats until halted 
+   loop {
+    println!("again!");
+   }
 }
