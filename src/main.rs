bfff72d6f9b745446bf3e5df48c5ef4fe328bcd4
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-06 08:23:14 +0300

    used if on the right side of an expression

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
-    let number = 3;
-    //conditions in if statements are sometimes referred to as arms
-    //when else if is used, program stops the moment the first true condition is met 
-    if number < 5 {
-        println!("the condition is true!");
-    }if number != 0 {
-        println!("the number is not equal to zero!");
+    let condition = true;
+
+    let number = if condition {
+        5
     }else{
-        println!("the condition is false!");
-    }
+        6
+    };
+
+    println!("the value of number is: {}", number);
 }
